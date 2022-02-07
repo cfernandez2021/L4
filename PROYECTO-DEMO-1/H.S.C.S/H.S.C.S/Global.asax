6c567855248a2c0e6051a673bf44ed76103c71cd
carlos fernandez <cfernandezd@unah.hn>
2022-02-07 12:36:09 -0600

    Proyecto Prueba

---
+++ PROYECTO-DEMO-1/H.S.C.S/H.S.C.S/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="H.S.C.S.MvcApplication" Language="C#" %>
---
+++ PROYECTO-DEMO-1/H.S.C.S/H.S.C.S_WEB/Controllers/ProductoController.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace H.S.C.S_WEB.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+
+            var productosBL = new ProductosBL();
+            var listadoProductos = productosBL.ObtenerProductos();
+
+            
+            return View(listadoProductos);
+        }
+    }
+}
